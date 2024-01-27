099522952d7f0877a5145d63d62b4d1dececfe78
Fausto <faustocecarosideveloper@gmail.com>
2024-01-27 13:06:30 -0300

    change db for db prod

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -82,7 +82,6 @@ const Note = ({ note }: NoteProps) => {
                         </>
                     )
                 }
-
               <DeleteBtn 
                 id={Number(note.id)}
               />
